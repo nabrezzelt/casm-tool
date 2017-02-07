565b78bf0ef96e4b4ec171c18d44207afaaa5c1c
nabrezzelt <nabrezzelt@users.noreply.github.com>
2017-02-07 10:09:27 +0100

    Update connect.inc.php

--- includes/connect.inc.php
+++ includes/connect.inc.php
@@ -2,10 +2,10 @@
     /*
     session_start();        
     
-    $host = "localhost";
-    $username = "root";
-    $password = "ascent";
-    $database = "casm-tool";    
+    $host = "";
+    $username = "";
+    $password = "";
+    $database = "";    
 
     $connection = mysql_connect($host, $username, $password, $database);
     $connectedDatabase = mysql_select_db($database) or die ("<p>Datenbank nicht gefunden oder fehlerhaft</p>");
